830e66bf0295eae3b83a9dbc3cf73cdf17625537
David Ivain <32299050+david-ivain@users.noreply.github.com>
2022-11-30 22:04:41 +0100

    feature: added tag list to projects refactor: restructured script files + extracted constants in front js for components

--- src/components/blocks/sections/Projects.ts
+++ src/components/blocks/sections/Projects.ts
@@ -1,10 +1,24 @@
-export default {
+const data = {
+    action: {
+        enlargeProjects: '[data-action="enlargeProjects"]'
+    },
+    elements: {
+        projects: '.s-projects'
+    },
+    classes: {
+        enlarged: 'enlarged'
+    }
+} as const;
+
+const Projects = {
     init() {
-        document.querySelector("[data-action=\"enlargeProjects\"]").addEventListener("click", onProjectsEnlarge);
+        document.querySelector(data.action.enlargeProjects).addEventListener("click", onProjectsEnlarge);
     }
 }
 
 function onProjectsEnlarge() {
-    document.querySelector("[data-action=\"enlargeProjects\"]").classList.toggle("open");
-    document.querySelector(".s-projects").classList.toggle("open");
+    document.querySelector(data.action.enlargeProjects).classList.toggle(data.classes.enlarged);
+    document.querySelector(data.elements.projects).classList.toggle(data.classes.enlarged);
 }
+
+export default Projects;
